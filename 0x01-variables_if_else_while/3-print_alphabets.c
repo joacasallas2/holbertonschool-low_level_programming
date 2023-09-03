f9c7ca0409dfa1c2a52159e83c1b749fdfe64421
joacasallas2 <proyecta.ds@gmail.com>
2023-09-03 17:07:13 -0500

    prints the alphabet in lowercase, and then in uppercase (putchar)

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-  * main - program that prints the alphabet in lowercase
+  * main - program that prints the alphabet in lowercase, and then in uppercase
   *
   * Return: Always 0 (Success)
   */
@@ -10,6 +10,10 @@ int main(void)
 	int i;
 
 	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i < 91; i++)
 	{
 		putchar(i);
 	}
