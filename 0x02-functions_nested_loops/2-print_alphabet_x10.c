e314e6fa9862fa270bfd47fb7888a83305153bcb
joacasallas2 <proyecta.ds@gmail.com>
2023-09-07 15:19:41 -0500

    2-print_alphabet_x10.c:  prints 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+/**
+  * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+  * @c: The character to print
+  * Return: void
+  */
+int _putchar(char c);
+
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 1; j > 0 && j < 11; j++)
+	{
+		for (i = 97; i > 96 && i < 123; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
+
