d757181af62afdf35f172c9ec26f4f3ffb542aa2
joacasallas2 <proyecta.ds@gmail.com>
2023-09-07 16:16:07 -0500

    5-sign.c: prints the sign of a number.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+/**
+  * print_sign - prints the sign of a number.
+  * @c: The character to print
+  * @n: The number to print sign
+  * Return: 1 if n is positive, -1 if n is negative or 0 if n == zero
+  */
+int _putchar(char c);
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('+');
+		return (1);
+	}
+}
