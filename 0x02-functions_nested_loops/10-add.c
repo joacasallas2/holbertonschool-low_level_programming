41b2201d8bd8a0ba29a8943ef55c3de7ca1d65c2
joacasallas2 <proyecta.ds@gmail.com>
2023-09-08 15:31:26 -0500

    10-add.c : function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+  * add - adds two integers
+  * @a: The first integer to sum
+  * @b: The second integer to sum
+  * Return: The result of the sum
+  */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
+
