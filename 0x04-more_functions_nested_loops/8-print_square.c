04fb312e92e5b86fc5339134ec279be39db3a24d
joacasallas2 <proyecta.ds@gmail.com>
2023-09-19 04:20:41 -0500

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+/**
+  * print_square - function that prints a square
+  * @size: the size of the square
+  * @c: The character to print
+  * Return: Nothing (void function)
+  */
+
+int _putchar(char c);
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
