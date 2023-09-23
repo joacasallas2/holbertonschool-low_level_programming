2cc204f69f7b28efdeb9a39056116dea24d475f0
joacasallas2 <proyecta.ds@gmail.com>
2023-09-23 07:18:03 -0500

    2-strlen.c: function that returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+/**
+  * _strlen - function that returns the length of a string.
+  * @s: the string to return the length
+  * Return: The length of a string
+  */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 1; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+
