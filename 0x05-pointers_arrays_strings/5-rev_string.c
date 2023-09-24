ea2cf4a361066dee06ed016f0504a23eaa4ef79e
joacasallas2 <proyecta.ds@gmail.com>
2023-09-24 02:52:52 -0500

    fix len string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,37 +1,22 @@
 /**
   * rev_string - function that reverses a string.
   * @s: The pointer to string to reverse
-  * Return: Nothing
+  * Return: Nothing (void function)
   */
-int _strlen(char *s);
 
 void rev_string(char *s)
 {
 	int i, j;
-	char *s2[_strlen(s)];
-
-	for (i = 0, j = 0; s[i] != '\0'; i++, j++)
-	{
-	}
-	for (i = 0; j >= 0; j--, i++)
-	{
-		*s[i] = s2[j];
-	}
-}
-
-
-/**
- * _strlen - function that returns the length of a string.
- * @s: the string to return the length
- * Return: The length of a string
- */
-int _strlen(char *s)
-{
-	int i;
+	char s2[256];
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		s2[i] = s[i];
 	}
-	return (i);
+	i--;
+	for (j = 0; i >= 0; j++, i--)
+	{
+		s[j] = s2[i];
+	}
 }
 
