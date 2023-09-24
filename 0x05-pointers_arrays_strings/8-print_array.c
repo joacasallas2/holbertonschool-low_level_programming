496503c6af0451c3ea21188a4dc877854e9f18e9
joacasallas2 <proyecta.ds@gmail.com>
2023-09-24 11:25:45 -0500

    8-print_array.c: fix output

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -18,18 +18,15 @@ void print_array(int *a, int n)
 	{
 		printf("\n");
 	}
-	else
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
+		if (i == (n - 1))
 		{
-			if (i == (n - 1))
-			{
-				printf("%d\n", a[i]);
-			}
-			else
-			{
-				printf("%d, ", a[i]);
-			}
+			printf("%d\n", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
 		}
 	}
 }
