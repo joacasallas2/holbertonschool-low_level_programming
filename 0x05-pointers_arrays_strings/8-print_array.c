17d758090ce11c5e4a06792882300bedebd85010
joacasallas2 <proyecta.ds@gmail.com>
2023-09-24 14:01:08 -0500

    8-print_array.c :  function that prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -17,12 +17,13 @@ void print_array(int *a, int n)
 	if (i != n)
 	{
 		printf("\n");
+		return;
 	}
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
 		{
-			printf("%d", a[i]);
+			printf("%d\n", a[i]);
 		}
 		else
 		{
