ab3fbcfe3bb52af8781d83670acb93465bc4d47d
joacasallas2 <proyecta.ds@gmail.com>
2023-09-29 10:14:17 -0500

    1-strncat.c: concatenates two strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; j < n; j++)
+	for (j = 0; (j < n) & (src[j] != '\0'); j++, i++)
 	{
 		ptr[i] = src[j];
 	}
