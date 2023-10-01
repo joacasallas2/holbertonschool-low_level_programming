222dce4af54bee1cc9c7b1ed6b33b1de4d869e88
joacasallas2 <proyecta.ds@gmail.com>
2023-09-30 19:06:09 -0500

    6-cap_string.c:  function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+/**
+  * cap_string -   function that capitalizes all words of a string.
+  * @ptr: The pointer to the string
+  * Return: A pointer to the resulting string dest
+  */
+char *cap_string(char *ptr)
+{
+	int i;
+
+	for (i = 1; ptr[i]; i++)
+	{
+		if (ptr[i] == '\t')
+		{
+			ptr[i] = ' ';
+		}
+		if ((ptr[i - 1] == ' ') || (ptr[i - 1] == '\n') || (ptr[i - 1] == '.'))
+		{
+			if (ptr[i] > 96 && ptr[i] < 123)
+			{
+				ptr[i] = ptr[i] - 32;
+			}
+		}
+	}
+	return (ptr);
+}
+
