59381664db874cebf64d70a7a71af0f0f3db84fa
joacasallas2 <proyecta.ds@gmail.com>
2023-10-04 04:18:17 -0500

    7-leet.c: function that encodes a string into 1337.

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,11 +1,25 @@
 /**
-  * _strcat -  function that concatenates two strings
-  * This appends the src string to the dest string, overwriting the terminating null byte (\0) 
-  * at the end of dest, and then adds a terminating null byte
-  * @dest: The pointer to destine string
-  * @src: The pointer to source string
-  * Return: A pointer to the resulting string dest
+  * leet -  function that encodes a string into 1337.
+  *
+  * @str: The pointer to the string
+  * Return: A pointer to the resulting string
   */
+char *leet(char *str)
+{
+	char *arr1 = "aeotl";
+	char *arr2 = "AEOTL";
+	char arr3[5] = {4, 3, 0, 7, 1};
+	int i, j;
 
-
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == arr1[j] || str[i] == arr2[j])
+			{
+				str[i] = 48 + arr3[j];
+			}
+		}
+	}
+	return (str);
+}
