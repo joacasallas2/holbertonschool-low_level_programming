d056eb5e008468c918ed767994d0eaaa05137a66
joacasallas2 <proyecta.ds@gmail.com>
2023-10-04 14:44:05 -0500

    101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,11 +1,41 @@
 /**
-  * _strcat -  function that concatenates two strings
-  * This appends the src string to the dest string, overwriting the terminating null byte (\0) 
-  * at the end of dest, and then adds a terminating null byte
-  * @dest: The pointer to destine string
-  * @src: The pointer to source string
-  * Return: A pointer to the resulting string dest
-  */
+ * print_number - function that prints an integer.
+ * @c: The character to print
+ * n: The number to print
+ * Return: Nothing (void function)
+ */
+int _putchar(char c);
 
+void print_number(int n)
+{
+	int number, div;
+	
+	number = n / 10;
+	div = 1;
 
-
+	if (n < 0)
+	{
+		number = number * -1;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (; number >= 1; number /= 10, div *= 10)
+	{
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	for (; div >= 1; div /= 10)
+	{
+		if (n / div < 1)
+		{
+			_putchar('0' + n);
+		}
+		_putchar('0' + (n / div) % 10);
+	}
+}
