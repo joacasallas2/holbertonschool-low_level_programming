2c8a36b9836a5ad14eb84783697b81a55a730221
joacasallas2 <proyecta.ds@gmail.com>
2023-10-04 11:27:32 -0500

    100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,25 @@
 /**
-  * _strcat -  function that concatenates two strings
-  * This appends the src string to the dest string, overwriting the terminating null byte (\0) 
-  * at the end of dest, and then adds a terminating null byte
-  * @dest: The pointer to destine string
-  * @src: The pointer to source string
-  * Return: A pointer to the resulting string dest
+  * rot13 -  function that encodes a string using rot13.
+  * @str: The pointer to the string
+  * Return: A pointer to the resulting string
   */
 
+char *rot13(char *str)
+{
+	char arr1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char arr2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == arr1[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
