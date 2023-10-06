8c275795bc2060e1eb3a829dcc819000a234369c
joacasallas2 <proyecta.ds@gmail.com>
2023-10-06 00:07:35 -0500

    102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,11 +1,23 @@
-/**
-  * _strcat -  function that concatenates two strings
-  * This appends the src string to the dest string, overwriting the terminating null byte (\0) 
-  * at the end of dest, and then adds a terminating null byte
-  * @dest: The pointer to destine string
-  * @src: The pointer to source string
-  * Return: A pointer to the resulting string dest
-  */
+#include <stdio.h>
 
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
 
-
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
