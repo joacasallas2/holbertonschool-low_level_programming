0f9fffee2c1858cf064db2fcb2b64f074de3f6eb
joacasallas2 <proyecta.ds@gmail.com>
2023-10-11 15:44:05 -0500

    103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -1,72 +1,68 @@
 /**
  * infinite_add - function that adds two numbers.
- * n1: The pointer to the first number
- * @n2: The pointer to the second number
+ * @n1: The pointer to the first number
+ * n2: The pointer to the second number
  * r: The buffer to store the result
  * size_r: The buffer size
- * Return: A pointer to the result
+ * Return: A pointer to the buffer r
  */
 
-int mult(char *n2);
-int num(char *n1, int mult1);
-
+int rec(char *n1);
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i;
-	long int mult1, mult2, num1, num2, res, div;
+	int i, j, k, pend, num1, num2, res;
 
-	num1 = num2 = res = 0;
-	mult1 = mult(n1);
-	num1 = num(n1, mult1);
-	mult2 = mult(n2);
-	num2 = num(n2, mult2);
-	res = num1 + num2;
-	for (div = 1; (res / div) > 1; div *= 10)
-	{
-	}
-	for (i = 0; div > 1; i++, div /= 10)
-	{
-		r[i] = 48 + (res / div) % 10;
-	}
-	r[i] = 48 + (res / div) % 10;
-	if (i >= (size_r - 1))
-	{
+	pend = num1 = num2 = 0;
+	i = rec(n1);
+	j = rec(n2);
+	if ((size_r - 1) < i || (size_r - 1) < j)
 		return (0);
+	i--;
+	j--;
+	k = size_r - 1;
+	r[k] = '\0';
+	k--;
+	for (; k >= -1; k--, i--, j--)
+	{
+		num1 = (n1[i] - 48);
+		num2 = (n2[j] - 48);
+		if (i < 0)
+			num1 = 0;
+		if (j < 0)
+			num2 = 0;
+		res = num1 + num2 + pend;
+		pend = 0;
+		if (res > 9)
+		{
+			pend = res / 10;
+			res = res % 10;
+		}
+		r[k] = res + 48;
 	}
+	if (k <= -1 && res > 0)
+		return (0);
+	for (i = 0; r[i] == 48; i++)
+	{
+	}
+	for (j = 0; r[i] != '\0'; i++, j++)
+	{
+		r[j] = r[i];
+	}
+	r[j] = '\0';
 	return (r);
 }
 
 /**
- * mult - function that get the mult of a number.
- * @n2: The pointer to the number
- * Return: the mult number
+ * rec - Function that get the lenght of a string.
+ * @n1: The pointer to the string
+ * Return: the large
  */
-
-int mult(char *n2)
+int rec(char *n1)
 {
-	long int i, mult;
+	int large;
 
-	for (i = 1, mult = 1; n2[i]; i++, mult *= 10)
+	for (large = 0; n1[large]; large++)
 	{
 	}
-	return (mult);
-}
-
-/**
- * num - function that get the int of a char.
- * @n1: The pointer to the number
- * @mult1: mult
- * Return: the integer
- */
-int num(char *n1, int mult1)
-{
-	long int num1 = 0;
-	int i;
-
-	for (i = 0; mult1 > 1; i++, mult1 /= 10)
-	{
-		num1 += (n1[i] - 48) * mult1;
-	}
-	num1 += n1[i] - 48;
-	return (num1);
+	return (large);
 }
