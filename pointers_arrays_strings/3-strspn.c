fef7aa4db7e3b52afbe2e6fa4ad7f8bbde9826df
joacasallas2 <proyecta.ds@gmail.com>
2023-10-19 15:02:19 -0500

    3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+/**
+ * _strspn - function that gets the length of a prefix substring
+ * @s: The pointer to the substring
+ * @accept: The pointer to the substring accepted
+ * Return: The number of bytes in s which consist only of bytes from accept
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	unsigned int len = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] == accept[0])
+		{
+			if (accept[0] == ' ')
+			{
+				continue;
+			}
+			break;
+		}
+	}
+	if (s[i - 1] != ' ')
+	{
+		for (; s[i] != ' ' && i >= 0; i--)
+		{
+		}
+		i++;
+	}
+
+	for (; s[i] != ' ' && s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	return (len - 1);
+}
