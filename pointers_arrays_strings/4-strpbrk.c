f5d163826df41321a54f163e625b3b7985884a86
joacasallas2 <proyecta.ds@gmail.com>
2023-10-20 16:24:48 -0500

    4-strpbrk.c: function that searches a string for any of a set of bytes.

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,5 +2,24 @@
  * _strpbrk -  function that searches a string for any of a set of bytes
  * @s: The string where to search
  * @accept: The substring to search
- * Return: A pointer to the byte in s that matches one of the bytes in accept 
-*/
+ * Return: A pointer to the byte in s that matches one of the bytes in accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *ptr = '\0';
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				ptr = &s[i];
+				return (ptr);
+			}
+		}
+	}
+	return (ptr);
+}
