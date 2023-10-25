0c4e4db274298f60d9f1420f776e9cbd828f109c
joacasallas2 <proyecta.ds@gmail.com>
2023-10-25 10:09:38 -0500

    4-pow_recursion.c: function that returns the value of x raised to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+/**
+ * _pow_recursion - function that returns the value of x
+ * raised to the power of y
+ * @x: the base number
+ * @y: exponential
+ * Return: The result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, (y - 1)));
+}
