e16dfb592c5d869ea2073e6478b538a4eaa93196
joacasallas2 <proyecta.ds@gmail.com>
2023-10-29 03:03:19 -0500

    3-mul.c: program that multiplies two numbers

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,72 @@
+#include "main.h"
+int _atoi(char *s);
+/**
+ * main -  program that multiplies two numbers
+ * @argc: The number of arguments
+ * @argv: The pointer to the array of strings
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		mul = mul * _atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
+
+/**
+  * _atoi -  function that convert a string to an integer
+  * @s: The Pointer to the string
+  * Return: the number or 0 if are not numbers
+  */
+int _atoi(char *s)
+{
+	int i, index, len, mul, number, sign, found, maxReached;
+
+	found = number = maxReached = 0;
+	mul = sign = 1;
+
+	for (index = 0; s[index] != '\0'; index++)
+	{
+		if (s[index] < 48 || s[index] > 57)
+			continue;
+		else
+		{
+			found = 1;
+			break;
+		}
+	}
+	if (found == 0)
+		return (0);
+	if (index != 0)
+		for (i = index; i >= 0; i--)
+		{
+			if (s[i] == '-')
+				sign *= -1;
+		}
+	for (len = index; s[len] != '\0'; len++)
+	{
+		if (s[len] < 48 || s[len] > 57)
+			break;
+		if (mul == 1000000000)
+		{
+			maxReached = 1;
+			break;
+		}
+		mul *= 10;
+	}
+	if (maxReached == 0)
+		mul = mul / 10;
+	for (; index <= len; index++, mul /= 10)
+		number += (((s[index] - '0') * mul) * sign);
+	return (number);
+}
