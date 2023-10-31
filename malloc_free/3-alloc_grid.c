3ead375417b37d09c35890bcd8ea9d9de6edd6a0
joacasallas2 <proyecta.ds@gmail.com>
2023-10-31 11:27:36 -0500

    3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	p = malloc(sizeof(int *) * height + 1);
+	p = malloc(sizeof(int *) * height);
 	if (p == NULL)
 	{
 		return (NULL);
