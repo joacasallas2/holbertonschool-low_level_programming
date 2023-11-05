f024e56ed25fa87e3dec584668a567a0e91f43ab
joacasallas2 <proyecta.ds@gmail.com>
2023-11-05 06:13:33 -0500

    101-strtow.c

--- malloc_free/101-strtow.c
+++ malloc_free/101-strtow.c
@@ -6,26 +6,47 @@
  */
 char **strtow(char *str)
 {
-	int i, j, k, numStr;
+	int i, j, k, l, numStr;
 	char **ptr;
 
-	numStr = j = k = 0;
-	if (str == NULL)
-		return (NULL);
+	j = k = l = numStr = 0;
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		while (str[i] == ' ')
+		{
+			i++;
+		}
+		if (str[i] != ' ' && str[i] != '\0')
+		{
 			numStr++;
+			for (; str[i] != ' ' && str[i]; i++)
+			{
+			}
+		}
+		if (str == NULL || numStr == 0)
+		{
+			return (NULL);
+		}
+		if (str[i] == '\0')
+		{
+			break;
+		}
 	}
 	ptr = (char **)malloc(sizeof(char *) * (numStr + 1));
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
+		while (str[i] == ' ')
 		{
 			i++;
-			for (; str[i] != ' '; i++, j++)
+		}
+		if (str[i] != ' ' && str[i] != '\0')
+		{
+			l = i;
+			for (; str[i] != ' ' && str[i] != '\0'; i++, j++)
 			{
 			}
 			ptr[k] = (char *)malloc(sizeof(char) * (j + 1));
@@ -38,27 +59,20 @@ char **strtow(char *str)
 				free(ptr);
 				return (NULL);
 			}
-			k++;
 			j = 0;
-			i--;
-		}
-	}
-	k = j = 0;
-	for (i = 0; str[i]; i++)
-	{
-		if (str[i] == ' ' && str[i + 1] != ' ' && str[i + 1] != '\0')
-		{
-			i++;
-			for (; str[i] != ' '; i++, j++)
+			for (; str[l] != ' ' && str[l] != '\0'; l++, j++)
 			{
-				ptr[k][j] = str[i];
+				ptr[k][j] = str[l];
 			}
 			ptr[k][j] = '\0';
-			k++;
 			j = 0;
-			i--;
+			k++;
+			if (k == numStr)
+			{
+				ptr[k] = NULL;
+				break;
+			}
 		}
 	}
-	ptr[k] = NULL;
 	return (ptr);
 }
