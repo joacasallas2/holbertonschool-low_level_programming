3c82aea035aebab9424061790c82ce075968d51a
joacasallas2 <proyecta.ds@gmail.com>
2023-11-06 15:34:52 -0500

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *ptr;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+		n = 0;
+	}
 	for (i = 0; s1[i]; i++)
 	{
 	}
-	ptr = (char *)malloc(sizeof(char) * (i + n));
+	ptr = (char *)malloc(sizeof(char) * (i + n + 1));
 	if (ptr == NULL)
 	{
 		return (NULL);
