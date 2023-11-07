051194fb9331f0087e2bbbce1fecc83f4ad8b669
joacasallas2 <proyecta.ds@gmail.com>
2023-11-07 05:27:56 -0500

    100-realloc.c

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	char *ptr2;
 	unsigned int i;
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
