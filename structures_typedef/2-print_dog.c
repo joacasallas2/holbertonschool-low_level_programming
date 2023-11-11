c1d149a055eb0b1e93d8d14dc6823efa4a749c35
joacasallas2 <proyecta.ds@gmail.com>
2023-11-11 17:10:12 -0500

    2-print_dog.c

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,23 +13,16 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL)
 	{
-		printf("(nil)\n");
+		printf("Name: (nil)\n");
 	}
 	else
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == '\0')
-	{
-		printf("(nil)\n");
-	}
-	else
-	{
-		printf("Age: %.6f\n", d->age);
-	}
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 	{
-		printf("(nil)\n");
+		printf("Owner: (nil)\n");
 	}
 	else
 	{
