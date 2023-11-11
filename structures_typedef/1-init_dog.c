f3baf2519ecaba592899ee6871d1c3ae0ca336d5
joacasallas2 <proyecta.ds@gmail.com>
2023-11-11 16:26:41 -0500

    1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,6 +12,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->age = age;
 	d->name = name;
 	d->owner = owner;
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+#include <stdio.h>
 
 /**
  * struct dog - Define a new type struct dog with the following elements
