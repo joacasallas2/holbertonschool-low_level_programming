8489519db3e47b4a8ed530219b21d51eff5f305e
joacasallas2 <proyecta.ds@gmail.com>
2023-11-11 13:23:05 -0500

    dog.h: Define a new type struct dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type struct dog with the following elements
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
