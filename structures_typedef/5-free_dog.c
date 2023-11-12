24fe1c6495dfa9ba58b19a6e4be1b08563e63e57
joacasallas2 <proyecta.ds@gmail.com>
2023-11-12 18:44:00 -0500

    5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs.
+ * @d: The pointer to the struct to free
+ * Return: Nothing (void function)
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
