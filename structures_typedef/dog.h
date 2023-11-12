297b63aefcd1128404f72b3a882f9f4cafb73b0e
joacasallas2 <proyecta.ds@gmail.com>
2023-11-12 06:38:58 -0500

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,8 +14,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - new name for struct dog
+*/
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 
 #endif
