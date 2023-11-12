db5bfa9e6dca69d72e0d9bba795ed97e96f89c2c
joacasallas2 <proyecta.ds@gmail.com>
2023-11-12 13:03:53 -0500

    4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-
+char *_strcpy(char *dest, char *src);
 /**
  * new_dog -  function that creates a new dog.
  * @name: The name
@@ -10,14 +10,54 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *name2, *owner2;
+	int age2, i;
 
 	new_dog = malloc(sizeof(*new_dog));
 	if (new_dog == NULL)
 	{
 		return (NULL);
 	}
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	for (i = 0; name[i]; i++)
+	{
+	}
+	name2 = malloc(sizeof(char *) * i);
+	if (name2 == NULL)
+	{
+		return (NULL);
+	}
+	name2 = _strcpy(name2, name);
+	for (i = 0; owner[i]; i++)
+	{
+	}
+	owner2 = malloc(sizeof(char *) * i);
+	if (owner2 == NULL)
+	{
+		return (NULL);
+	}
+	owner2 = _strcpy(owner2, owner);
+	age2 = age;
+	new_dog->name = name2;
+	new_dog->age = age2;
+	new_dog->owner = owner2;
 	return (new_dog);
+}
+
+/**
+ * _strcpy - function that copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: The pointer to the buffer to store the string copied
+ * @src: The pointer to the string to copy
+ * Return: The ponter to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
 }
