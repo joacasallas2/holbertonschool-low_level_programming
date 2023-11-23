04dc1398e987bbbdbecb2859a1854c77ce321ac4
joacasallas2 <proyecta.ds@gmail.com>
2023-11-23 04:44:54 -0500

    3-print_all.c:  function that prints anything.

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,59 +1,119 @@
 #include "variadic_functions.h"
 
-void func1(char s, va_list args);
-
 /**
  * print_all - function that prints anything.
  * @format: list of types of arguments passed to the function
  * Return: Nothing (void function)
  */
+
 void print_all(const char *const format, ...)
 {
 	va_list args;
 	char buffer[BUFSIZ];
-	int numArgs, i, j, flag = 0;
-	char array[] = "cifs";
+	int i, numArgs;
+	int (*fun_ptr)(va_list args);
 
 	va_start(args, format);
 	numArgs = vsnprintf(buffer, BUFSIZ, format, args);
-
 	i = 0;
 	while (format[i])
 	{
-		j = 0;
-		while (array[j])
+		fun_ptr = get_op_func(format[i]);
+		if (fun_ptr == NULL)
 		{
-			if (format[i] == array[j])
-			{
-				func1(format[i], args);
-				flag = 1;
-			}
-			if (flag == 1 && i != numArgs - 1)
+			i++;
+		}
+		else
+		{
+			fun_ptr(args);
+			if (i < numArgs - 1)
 			{
 				printf(", ");
 			}
-			flag = 0;
-			j++;
+			i++;
 		}
-		i++;
 	}
 	printf("\n");
 }
 
 /**
- * func1 - function that prints anything.
- * @s: format passed
- * @args: argument passed
- * Return: Nothing (void function)
+ * get_op_func - function that selects the correct function
+ * @s: arguments passed to the function
+ * Return: The pointer to the function
  */
-void func1(char s, va_list args)
+int (*get_op_func(char s))(va_list args)
 {
-	if (s == 'c')
-		printf("%c", va_arg(args, int));
-	else if (s == 'i')
-		printf("%i", va_arg(args, int));
-	else if (s == 'f')
-		printf("%f", (double)va_arg(args, int));
-	else if (s == 's')
-		printf("%s", va_arg(args, char *));
+	op_t ops[] = {
+	    {"c", getChar},
+	    {"i", getInt},
+	    {"f", getFloat},
+	    {"s", getString},
+	};
+	int i = 0;
+
+	while (i < 4)
+	{
+		if (*ops[i].op == s)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * getChar - Function that prints a char
+ * @args: The list of args to run
+ * Return: Always 0 (Success)
+ */
+int getChar(va_list args)
+{
+	int element;
+
+	element = va_arg(args, int);
+	printf("%c", element);
+	return (0);
+}
+/**
+ * getInt - Function that prints an integer
+ * @args: The list of args to run
+ * Return: Always 0 (Success)
+ */
+int getInt(va_list args)
+{
+	int element;
+
+	element = va_arg(args, int);
+	printf("%i", element);
+	return (0);
+}
+/**
+ * getFloat - Function that prints a float
+ * @args: The list of args to run
+ * Return: Always 0 (Success)
+ */
+int getFloat(va_list args)
+{
+	int element;
+
+	element = va_arg(args, int);
+	printf("%f", (double)element);
+	return (0);
+}
+/**
+ * getString - Function that prints a String
+ * @args: The list of args to run
+ * Return: Always 0 (Success)
+ */
+int getString(va_list args)
+{
+	char *elementString;
+
+	elementString = va_arg(args, char *);
+	if (elementString == NULL)
+	{
+		elementString = "(nil)";
+	}
+	printf("%s", elementString);
+	return (0);
 }
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -14,7 +14,7 @@
 typedef struct op
 {
 	char *op;
-	int (*f)(va_list args, int);
+	int (*f)(va_list);
 } op_t;
 
 int _putchar(char c);
@@ -22,4 +22,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
+int getChar(va_list args);
+int getInt(va_list args);
+int getFloat(va_list args);
+int getString(va_list args);
+int (*get_op_func(char s))(va_list args);
+
 #endif
