bd7c370060a459467ca0322bc71ec63a888bc771
joacasallas2 <proyecta.ds@gmail.com>
2023-12-13 11:59:43 -0500

    function that prints all the elements of a list_t list.

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: The  pointer to the linked list
+ * Return: The number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+
+	while (h->next != NULL)
+	{
+		printf("[%d] ", h->len);
+		if (h->str == NULL)
+		{
+			printf("nil\n");
+		}
+		else
+		{
+			printf("%s\n", h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	printf("[%d] %s\n", h->len, h->str);
+	i++;
+	return (i);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LIST_H
+#define LIST_H
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,6 +21,11 @@ typedef struct list_s
 } list_t;
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 
 
