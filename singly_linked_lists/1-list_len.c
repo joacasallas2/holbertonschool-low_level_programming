df29485c60d9170f34983e8ad4ee86c69510095f
joacasallas2 <proyecta.ds@gmail.com>
2023-12-13 16:43:20 -0500

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * list_len -  function that returns the number of elements
+ * in a linked list_t list.
+ * @h: The pointer to the single linked list
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h->next)
+	{
+		h = h->next;
+		i++;
+	}
+	i++;
+	return (i);
+}
