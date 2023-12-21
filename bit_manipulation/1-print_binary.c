c7f533087d2ab1319aa39705befacf4aec21ac89
joacasallas2 <proyecta.ds@gmail.com>
2023-12-21 05:49:31 -0500

    1-print_binary.c

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -6,10 +6,32 @@
  */
 void print_binary(unsigned long int n)
 {
-	
+	unsigned long int num = 0, res = 0;
+
 	if (n > 1)
 	{
-		print_binary(n >> 2);
+		print_binary(n >> 1);
 	}
-	_putchar('0' + (n & 1));
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
+	if (n == 1)
+	{
+		printf("1");
+		return;
+	}
+	while ((num * 2) < n)
+	{
+		if (((num + 1) * 2) > n)
+		{
+			break;
+		}
+		num++;
+	}
+	res = n - (num * 2);
+	printf("%ld", res);
+	n = num;
+	num = 0;
 }
