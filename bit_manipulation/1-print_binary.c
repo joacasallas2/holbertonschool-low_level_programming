faa47226d20c554d720528f3b5849ad24c5d7999
joacasallas2 <proyecta.ds@gmail.com>
2023-12-22 04:34:01 -0500

    2-get_bit.c: function that returns the value of a bit at a given index.

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -11,5 +11,4 @@ void print_binary(unsigned long int n)
 		print_binary(n >> 1);
 	}
 	_putchar('0' + (n & 1));
-
 }
---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_bit - function that returns the value of a bit at a given index.
+ * @n:  The number
+ * @index: The index of the bit to get
+ * Return: The value of the bit at index or -1 if an error ocurred
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	while (index > 0)
+	{
+		index--;
+		n = n >> 1;
+	}
+	if (!n)
+	{
+		return (-1);
+	}
+	return (1 & n);
+}
