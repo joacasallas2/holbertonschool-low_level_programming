8108ae69918973c54e8b0ab5435baae2bfd96ba2
joacasallas2 <proyecta.ds@gmail.com>
2023-12-24 16:20:49 -0500

    Create a function that creates a file.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,16 +14,16 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 	{
 		return (-1);
 	}
 	wrote = write(fd, text_content, strlen(text_content));
-	if (wrote <= 0)
+	if (wrote <= 0 || wrote != strlen(text_content))
 	{
-		write(STDOUT_FILENO, "fails", strlen("fails"));
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
