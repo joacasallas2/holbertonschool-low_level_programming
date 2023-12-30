f8e9d5fd0200df6e713d133b075eb05180911db9
joacasallas2 <proyecta.ds@gmail.com>
2023-12-30 02:48:42 -0500

    8-delete_dnodeint.c

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -13,25 +13,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		return (-1);
 	}
+	while (index > 1)
+	{
+		if (temp == NULL)
+		{
+			return (-1);
+		}
+		temp = temp->next;
+		index--;
+	}
 	if (index == 0)
 	{
+		*head = (*head)->next;
 		if (*head != NULL)
 		{
 			(*head)->prev = NULL;
 		}
-		*head = (*head)->next;
 	}
 	else
 	{
-		while (index > 1)
-		{
-			if (temp == NULL)
-			{
-				return (-1);
-			}
-			temp = temp->next;
-			index--;
-		}
 		temp->prev->next = temp->next;
 		if (temp->next != NULL)
 		{
