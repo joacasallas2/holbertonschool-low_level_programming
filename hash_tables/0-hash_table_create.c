5a999eb1c9397e3291ddb828485a7491b5b3398d
joacasallas2 <proyecta.ds@gmail.com>
2024-02-22 18:16:06 -0500

    0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_table;
 
 	new_hash_table = malloc(sizeof(new_hash_table));
-	if (new_hash_table = NULL)
+	if (new_hash_table == NULL)
 	{
 		return (NULL);
 	}
