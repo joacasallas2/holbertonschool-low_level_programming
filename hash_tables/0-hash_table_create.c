7da28769efb562c6804053e5924f6acac4fdf490
joacasallas2 <proyecta.ds@gmail.com>
2024-02-22 18:10:05 -0500

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -  function that creates a hash table.
+ * @size: The size of the array
+ * Return: a pointer to the newly created hash table
+ * or NULL if something went wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+
+	new_hash_table->size = size;
+
+	return (new_hash_table);
+}
