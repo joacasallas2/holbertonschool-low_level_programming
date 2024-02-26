d0abc7bed758b2f42d312578c4fcae12018fbba2
joacasallas2 <proyecta.ds@gmail.com>
2024-02-26 07:19:07 -0500

    3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -39,7 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (ht->array[index] != NULL)
 	{
-		new_node->next = ht->array[index];
+		free(ht->array[index]);
 	}
 	ht->array[index] = new_node;
 	return (1);
